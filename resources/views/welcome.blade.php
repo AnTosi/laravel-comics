ca4619bcf1725fdd85e8ef27a874023fcdf7af02
AnTosi <andrea.tosi.uni@gmail.com>
2021-12-17 18:10:24 +0100

    do main title start comic container

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,4 +6,13 @@
 <div class="jumbotron">
     <img src="{{ 'img/jumbotron.jpg' }}" alt="">
 </div>
+
+<div class="bg-black">
+    <div class="container position-relative">
+        <span class="blue_title px-3 py-2">CURRENT SERIES</span>
+        <div class="comics_container my-3">
+
+        </div>
+    </div>
+</div>
 @endsection
