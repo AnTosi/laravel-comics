b10fad37cdaa2a658b9834990454320ba3a5982a
AnTosi <andrea.tosi.uni@gmail.com>
2021-12-17 18:52:28 +0100

    do main

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,51 @@
 <div class="bg-black">
     <div class="container position-relative">
         <span class="blue_title px-3 py-2">CURRENT SERIES</span>
-        <div class="comics_container my-3">
-
+        <div class="comics_container d-flex flex-wrap py-5">
+            @foreach ($comics as $comic)
+            <div class="col-2">
+                <div class="p-2">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                    <h6 class="text-white py-2">{{ strtoupper($comic['series']) }}</h6>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="d-flex justify-content-center py-3">
+            <button class="blue_button">LOAD MORE</button>
         </div>
     </div>
 </div>
+
+<div class="bg_main-blue py-5">
+    <div class="blueband_container container d-flex justify-content-around text_white">
+        <div class="col-20 d-flex align-items-center">
+            <img src="{{'img/buy-comics-digital-comics.png'}}" alt="">
+            <div class="d-flex px-2">DIGITAL COMICS</div>
+        </div>
+    
+        <div class="col-20 d-flex align-items-center">
+            <img src="{{'img/buy-comics-merchandise.png'}}" alt="">
+            <div class="d-flex px-2">DC MERCHANDISE</div>
+        </div>
+    
+        <div class="col-20 d-flex align-items-center">
+            <img src="{{'img/buy-comics-subscriptions.png'}}" alt="">
+            <div class="d-flex px-2">SUBSCRIPTION</div>
+        </div>
+    
+        <div class="col-20 d-flex align-items-center">
+            <img src="{{'img/buy-comics-shop-locator.png'}}" alt="">
+            <div class="d-flex px-2">COMIC SHOP LOCATOR</div>
+        </div>
+    
+        <div class="col-20 d-flex align-items-center">
+            <img src="{{'img/buy-dc-power-visa.svg'}}" alt="">
+            <div class="d-flex px-2">DC POWER VISA</div>
+        </div>
+    </div>
+
+</div>
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (){
-    return view('welcome');
+    $comics = config('comics');
+    return view('welcome', compact('comics'));
 })->name('welcome');
