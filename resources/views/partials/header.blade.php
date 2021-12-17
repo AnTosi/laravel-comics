b9f09b0aeeb0bc226c641c2ce36824528b47b2b4
AnTosi <andrea.tosi.uni@gmail.com>
2021-12-17 17:03:52 +0100

    start header, change favicon.ico

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header id="site_header">
+    <div class="bg_main-blue upper_band">
+        <div class="d-flex justify-content-end py-2 container">
+            <div class="px-5 semi-bold"> 
+                DC POWER&#8480; VISA&#174;
+            </div>
+            <div class="semi-bold">
+                ADDITIONAL DC SITES <span class="half-rem">&#9660;</span>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+            <img src="../img/dc-logo.png" alt="">
+    </div>
+</header>
